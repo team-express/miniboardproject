732b3f764fd322c899b6b42a8750842916bc8f77
kimjinwook1 <87925272+kimjinwook1@users.noreply.github.com>
2022-02-21 15:26:49 +0900

    initial commit

---
+++ src/main/java/express/miniboardproject/MiniboardprojectApplication.java
@@ -0,0 +1,13 @@
+package express.miniboardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniboardprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniboardprojectApplication.class, args);
+	}
+
+}
